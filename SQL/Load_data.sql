21d8d2e8a74889bf87f72327eea8d15123b86195
Adetunji Olaoluwa <olaoluwachibuzor@1999>
2026-02-28 12:38:44 +0100

    query to load data into database

---
+++ SQL/Load_data.sql
@@ -0,0 +1,2 @@
+-- CODE USE TO LOAD DATA INTO THE TABLE ON POSTGRESQL
+\copy sales_data FROM 'C:/Users/personal/Desktop/Moutiq - DACA/Git and GitHub/sales_analytics_capstone/data/sales_data.csv' DELIMITER ',' CSV;
