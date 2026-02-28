f4904aea97024713808e2f4cf0f5ce5545869fae
Adetunji Olaoluwa <olaoluwachibuzor@1999>
2026-02-28 12:54:01 +0100

    sql query for total sales

---
+++ SQL/01_total_sales.sql
@@ -0,0 +1,3 @@
+SELECT 
+    SUM(price * quantity ) AS total_sales
+FROM sales_data
---
+++ SQL/Create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sales_data (
+    order_date DATE,
+    segment VARCHAR(50),
+    country VARCHAR(100),
+    market VARCHAR(50),
+    order_id VARCHAR(30),
+    category VARCHAR(50),
+    product_name TEXT,
+    price NUMERIC(10,3),
+    quantity INTEGER
+);
