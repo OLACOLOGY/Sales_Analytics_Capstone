715d8d061b64f3989823dd52ec7bb4674c72a17c
Adetunji Olaoluwa <olaoluwachibuzor@1999>
2026-02-28 13:54:35 +0100

    total sales

---
+++ 01_total_sales.sql
@@ -0,0 +1,5 @@
+SELECT 
+    SUM(price * quantity ) AS total_sales
+FROM sales_data
+
+this query calculates the total sales by multiplying the price and quantity for each record in the sales_data table and then summing up those values to get the overall total sales. The result is returned as total_sales.
