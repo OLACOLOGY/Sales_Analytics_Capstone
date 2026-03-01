c647ca1e7da966897081e9eec77ea5db6e5a0d26
Adetunji Olaoluwa <olaoluwachibuzor@1999>
2026-03-01 12:15:37 +0100

    updated query - total item sold include

--- 07_sales_by_segment.sql
+++ 07_sales_by_segment.sql
@@ -1,7 +1,9 @@
 SELECT
     segment,
-    SUM(price * quantity ) AS total_sales
+    SUM(price * quantity ) AS total_sales,
+    COUNT(order_id) AS total_item_sold
 FROM sales_data
 GROUP BY segment
 ORDER BY total_sales DESC
 LIMIT 10;
+
